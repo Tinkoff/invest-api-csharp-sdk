3ea148d1a4d930e331bd52a8c020b53e3a073072
Vabka <vab-999@ya.ru>
2022-07-30 00:27:12 +0500

    Add example for MarketDataStream

---
+++ Examples/Tinkoff.InvestApi.Examples.MarketDataStream/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Tinkoff.InvestApi;
+using Tinkoff.InvestApi.V1;
+
+var token = Environment.GetEnvironmentVariable("TOKEN");
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddInvestApiClient((_, settings) =>
+{
+    settings.AccessToken = token;
+});
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var client = serviceProvider.GetRequiredService<InvestApiClient>();
+
+var stream = client.MarketDataStream.MarketDataStream();
+await stream.RequestStream.WriteAsync(new MarketDataRequest
+{
+    SubscribeCandlesRequest = new SubscribeCandlesRequest
+    {
+        Instruments =
+        {
+            new CandleInstrument
+            {
+                Figi = "BBG004730N88",
+                Interval = SubscriptionInterval.OneMinute
+            }
+        }
+    }
+});
+await foreach (var response in stream.ResponseStream.ReadAllAsync())
+{
+    Console.WriteLine(JsonSerializer.Serialize(response));
+}
