0d932979fe211a5c951c9f936fe07d9fc161e14d
v.zhukov <v.zhukov@tinkoff.ru>
2022-08-18 11:21:17 +0300

    Упрощение запуска примера. Рефакторинг

--- Examples/Tinkoff.InvestApi.Examples.MarketDataStream/Program.cs
+++ Examples/Tinkoff.InvestApi.Examples.MarketDataStream/Program.cs
@@ -6,12 +6,14 @@ using Tinkoff.InvestApi.V1;
 
 // Получаем токен из переменной окружения
 var token = Environment.GetEnvironmentVariable("TOKEN");
+// Или раскомментируйте строку ниже и замените <<TOKEN>> вашим токеном
+// var token = "<<TOKEN>>";
 
 // Собираем ServiceCollection с клиентом
 var serviceCollection = new ServiceCollection();
 serviceCollection.AddInvestApiClient((_, settings) =>
 {
-    settings.AccessToken = token;
+	settings.AccessToken = token;
 });
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
@@ -22,20 +24,20 @@ var stream = client.MarketDataStream.MarketDataStream();
 // Отправляем запрос в стрим
 await stream.RequestStream.WriteAsync(new MarketDataRequest
 {
-    SubscribeCandlesRequest = new SubscribeCandlesRequest
-    {
-        Instruments =
-        {
-            new CandleInstrument
-            {
-                Figi = "BBG004730N88",
-                Interval = SubscriptionInterval.OneMinute
-            }
-        }
-    }
+	SubscribeCandlesRequest = new SubscribeCandlesRequest
+	{
+		Instruments =
+		{
+			new CandleInstrument
+			{
+				Figi = "BBG004730N88",
+				Interval = SubscriptionInterval.OneMinute
+			}
+		}
+	}
 });
 // Обрабатываем все приходящие из стрима ответы
 await foreach (var response in stream.ResponseStream.ReadAllAsync())
 {
-    Console.WriteLine(JsonSerializer.Serialize(response));
+	Console.WriteLine(JsonSerializer.Serialize(response));
 }
