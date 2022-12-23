e6a83741d6d196f79a8bf2fa212afef7f834e3cc
Ruslan Artamonov <r.artamonov@tinkoff.ru>
2022-12-23 12:16:48 +0300

    Release 0.4.1 #48 Added Sandbox option to InvestApiClientFactory

--- Tinkoff.InvestApi/InvestApiClientFactory.cs
+++ Tinkoff.InvestApi/InvestApiClientFactory.cs
@@ -4,10 +4,27 @@ namespace Tinkoff.InvestApi;
 
 public static class InvestApiClientFactory
 {
-    public static InvestApiClient Create(string accessToken)
+    public static InvestApiClient Create(string accessToken, bool sandbox = false)
     {
         return new ServiceCollection()
-            .AddInvestApiClient((_, settings) => settings.AccessToken = accessToken)
+            .AddInvestApiClient((_, settings) =>
+            {
+                settings.AccessToken = accessToken;
+                settings.Sandbox = sandbox;
+            })
+            .BuildServiceProvider()
+            .GetRequiredService<InvestApiClient>();
+    }  
+    
+    public static InvestApiClient Create(InvestApiSettings settings)
+    {
+        return new ServiceCollection()
+            .AddInvestApiClient((_, clientSettings) =>
+            {
+                clientSettings.AccessToken = settings.AccessToken;
+                clientSettings.Sandbox = settings.Sandbox;
+                clientSettings.AppName = settings.AppName;
+            })
             .BuildServiceProvider()
             .GetRequiredService<InvestApiClient>();
     }
